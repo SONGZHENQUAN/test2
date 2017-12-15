3c13ccb3569b34819dbf2b8ad2dc90eb2f7e2c94
SONGZHENQUAN <564708201@qq.com>
2017-12-15 21:43:56 +0800

    江湖第一帅

---
+++ work.java
@@ -0,0 +1,19 @@
+package home;
+
+public class work {
+
+	public static void main(String[] args) {
+		int i = 0;
+		while(i < 10){
+			System.out.println(i);
+			if(i < 1){
+				break;
+			}
+			if(i == 5){
+				continue;
+			}
+			i++;
+		}
+		这是通过git上传的自己的代码
+	}
+}
